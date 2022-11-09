4922b4614c961fd1bbca523840e0c7219810d6a2
bijianing97 <826015751@qq.com>
2022-11-09 14:34:41 +0800

    chore: add datadir config

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,30 @@ import {
   DBTarget,
 } from "@rei-network/database";
 import { Common } from "@rei-network/common";
-import fs from "fs";
-import path from "path";
+import { datadirs } from "./config";
 
 async function main() {
-  const datadir = "";
-  const db = createLevelDB(datadir)[0];
   const common = new Common({ chain: "rei-devnet" });
-  const dbManager = new Database(db, common);
-  const dbOps: DBOp[] = [];
-  const toDeleteBlock = await dbManager.getBlock(7387545);
-  const lastBlock = await dbManager.getBlock(7387544);
-  const heads = await dbManager.getHeads();
-  console.log("heads value is : ", heads);
-  //   dbOps.push(
-  //     DBOp.del(DBTarget.HashToNumber, { blockHash: toDeleteBlock.hash() }),
-  //     DBOp.del(DBTarget.NumberToHash, {
-  //       blockNumber: toDeleteBlock.header.number,
-  //     }),
-  //     DBOp.del(DBTarget.Heads, { blockHash: toDeleteBlock.hash() }),
-  //     DBOp.set(DBTarget.HeadHeader, lastBlock.hash()),
-  //     DBOp.set(DBTarget.HeadBlock, lastBlock.hash())
-  //   );
+  for (const datadir of datadirs) {
+    const db = createLevelDB(datadir)[0];
+    const dbManager = new Database(db, common);
+    const dbOps: DBOp[] = [];
+    const toDeleteBlock = await dbManager.getBlock(7387545);
+    const lastBlock = await dbManager.getBlock(7387544);
+    const heads = await dbManager.getHeads();
+    console.log("heads value is : ", heads);
+    //   dbOps.push(
+    //     DBOp.del(DBTarget.HashToNumber, { blockHash: toDeleteBlock.hash() }),
+    //     DBOp.del(DBTarget.NumberToHash, {
+    //       blockNumber: toDeleteBlock.header.number,
+    //     }),
+    //     DBOp.del(DBTarget.Heads, { blockHash: toDeleteBlock.hash() }),
+    //     DBOp.set(DBTarget.HeadHeader, lastBlock.hash()),
+    //     DBOp.set(DBTarget.HeadBlock, lastBlock.hash())
+    //   );
 
-  //   await dbManager.batch(dbOps);
+    //   await dbManager.batch(dbOps);
+  }
 }
 
 main().catch((err) => {
