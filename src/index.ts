41cdf7ece008bc2d76efc1f383588826a8475e06
bijianing97 <826015751@qq.com>
2022-11-09 15:49:47 +0800

    chore: add fix

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
 } from "@rei-network/database";
 import { Common } from "@rei-network/common";
 import { datadirs } from "./config";
+import { BN } from "ethereumjs-util";
 
 async function main() {
   const common = new Common({ chain: "rei-testnet" });
@@ -15,15 +16,14 @@ async function main() {
     const dbManager = new Database(db, common);
     const dbOps: DBOp[] = [];
     const toDeleteBlock = await dbManager.getBlock(7387545);
-    const lastBlock = await dbManager.getBlock(7387544);
-    const heads = await dbManager.getHeads();
+    const lastBlockHash = await dbManager.numberToHash(new BN(7387544));
     dbOps.push(
       DBOp.del(DBTarget.HashToNumber, { blockHash: toDeleteBlock.hash() }),
       DBOp.del(DBTarget.NumberToHash, {
         blockNumber: toDeleteBlock.header.number,
       }),
-      DBOp.set(DBTarget.HeadHeader, lastBlock.hash()),
-      DBOp.set(DBTarget.HeadBlock, lastBlock.hash())
+      DBOp.set(DBTarget.HeadHeader, lastBlockHash),
+      DBOp.set(DBTarget.HeadBlock, lastBlockHash)
     );
 
     await dbManager.batch(dbOps);
