02d860e9bdac522a62d1aaeb3c138b59e018a4dd
bijianing97 <826015751@qq.com>
2023-06-13 14:45:57 +0800

    chore: add command line

---
+++ README.md
@@ -0,0 +1,9 @@
+## How to use
+
+for Example:
+
+```bash
+- npm install
+- npm run build
+- npm run delete -- --last-block-number 200 --datadir /root/gxchain2-testnet/validator2/chaindb,/mnt/validator5/chaindb,/root/gxchain2-testnet/validator1/chaindb
+```
--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,64 @@ import {
   DBTarget,
 } from "@rei-network/database";
 import { Common } from "@rei-network/common";
-import { datadirs } from "./config";
 import { BN } from "ethereumjs-util";
 
 async function main() {
+  const lastBlockNumber = Number(
+    process.argv[process.argv.indexOf("--last-block-number") + 1]
+  );
+  const datadirList =
+    process.argv[process.argv.indexOf("--datadir") + 1].split(",");
+  const lastBlockNumberBN = new BN(lastBlockNumber);
+  const confirmsBlockNumber = 256;
+  const bloomBitsSectionSize = 4096;
   const common = new Common({ chain: "rei-testnet" });
-  for (const datadir of datadirs) {
-    const db = createEncodingLevelDB(datadir)[0];
-    const dbManager = new Database(db, common);
-    const dbOps: DBOp[] = [];
-    let toDeleteBlockNumber = 7216471;
-    const lastBlockHash = await dbManager.numberToHash(new BN(7216470));
-    while (true) {
-      try {
-        const block = await dbManager.getBlock(toDeleteBlockNumber++);
-        dbOps.push(
-          DBOp.del(DBTarget.HashToNumber, { blockHash: block.hash() }),
-          DBOp.del(DBTarget.NumberToHash, {
-            blockNumber: block.header.number,
-          })
-        );
-      } catch (err) {
-        if (err.type === "NotFoundError") {
-          break;
+  try {
+    for (const datadir of datadirList) {
+      const db = createEncodingLevelDB(datadir)[0];
+      const dbManager = new Database(db, common);
+      const dbOps: DBOp[] = [];
+      let toDeleteBlockNumber = lastBlockNumber + 1;
+      const lastBlockHash = await dbManager.numberToHash(
+        new BN(lastBlockNumber)
+      );
+      while (true) {
+        try {
+          const block = await dbManager.getBlock(toDeleteBlockNumber++);
+          dbOps.push(
+            DBOp.del(DBTarget.HashToNumber, { blockHash: block.hash() }),
+            DBOp.del(DBTarget.NumberToHash, {
+              blockNumber: block.header.number,
+            })
+          );
+        } catch (err) {
+          if (err.type === "NotFoundError") {
+            break;
+          }
+          throw err;
         }
-        throw err;
       }
-    }
-    dbOps.push(
-      DBOp.set(DBTarget.HeadHeader, lastBlockHash),
-      DBOp.set(DBTarget.HeadBlock, lastBlockHash)
-    );
+      let currentSections: BN | undefined = lastBlockNumberBN.gtn(
+        confirmsBlockNumber
+      )
+        ? lastBlockNumberBN.subn(confirmsBlockNumber).divn(bloomBitsSectionSize)
+        : undefined;
+      currentSections = currentSections.gtn(0)
+        ? currentSections.subn(1)
+        : undefined;
+      if (currentSections) {
+        DBOp.set(DBTarget.BloomBitsSectionCount, currentSections);
+      }
+      dbOps.push(
+        DBOp.set(DBTarget.HeadHeader, lastBlockHash),
+        DBOp.set(DBTarget.HeadBlock, lastBlockHash)
+      );
 
-    await dbManager.batch(dbOps);
-    console.log("finished");
+      await dbManager.batch(dbOps);
+      console.log(`datadir ${datadir} finished`);
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
 
